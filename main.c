c798c03f04150f7652002704962dad5ed8779a20
JuliusDgenius <ibejulius1@gmail.com>
2023-07-27 14:00:01 +0100

    changes

--- main.c
+++ main.c
@@ -59,58 +59,58 @@ int main(int argc, char **argv)
  */
 char *read_cmd()
 {
-char buf[1024];
-char *ptr = NULL;
-char ptrlen = 0;
+	char buf[1024];
+	char *ptr = NULL;
+	char ptrlen = 0;
 
-while (fgets(buf, 1024, stdin))
-{
-int buflen = strlen(buf);
+	while (fgets(buf, 1024, stdin))
+	{
+		int buflen = strlen(buf);
 
-if (!ptr)
-{
-ptr = malloc(buflen + 1);
-}
-else
-{
-char *ptr2 = realloc(ptr, ptrlen + buflen + 1);
+		if (!ptr)
+		{
+			ptr = malloc(buflen + 1);
+		}
+		else
+		{
+			char *ptr2 = realloc(ptr, ptrlen + buflen + 1);
 
-if (ptr2)
-{
-ptr = ptr2;
-}
-else
-{
-free(ptr);
-ptr = NULL;
-}
-}
-if (!ptr)
-{
-fprintf(stderr, "error: failed to alloc buffer: %s\n",
-	strerror(errno));
-return (NULL);
-}
+			if (ptr2)
+			{
+				ptr = ptr2;
+			}
+			else
+			{
+				free(ptr);
+				ptr = NULL;
+			}
+		}
+		if (!ptr)
+		{
+			fprintf(stderr, "error: failed to alloc buffer: %s\n",
+			strerror(errno));
+			return (NULL);
+		}
 
-strcpy(ptr + ptrlen, buf);
+		strcpy(ptr + ptrlen, buf);
 
-if (buf[buflen - 1] == '\n')
-{
-if (buflen == 1 || buf[buflen - 2] != '\\')
-{
-return (ptr);
-}
+		if (buf[buflen - 1] == '\n')
+		{
+			if (buflen == 1 || buf[buflen - 2] != '\\')
+			{
+				return (ptr);
+			}
 
-ptr[ptrlen + buflen - 2] = '\0';
-buflen -= 2;
-print_prompt2();
-}
+			ptr[ptrlen + buflen - 2] = '\0';
+			buflen -= 2;
+			print_prompt2();
+		}
 
-ptrlen += buflen;
-}
+		ptrlen += buflen;
+	}
 
-return (ptr);
-}
+			return (ptr);
+	}
 
 /**
  * parse_and_execute - parses and executes simple commands
